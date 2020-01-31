ebee5da024fbcb0ff5ae5cd21cf674e7d6443b3a
quaziarafeen <shamseelmail1@gmail.com>
2020-01-31 16:32:30 -0500

    Revised Project

---
+++ AmericanAirlines/src/main/java/home/webelement/WebElementHomePage.java
@@ -0,0 +1,23 @@
+package home.webelement;
+
+public class WebElementHomePage {
+
+
+// All the Web Elements comes in this class from HomePage
+
+
+    public static final String webElementSearchBox="//*[@id=\"twotabsearchtextbox\"]";
+    public static final String webElementSearchButton="//*[@id=\"nav-search\"]/form/div[2]/div/input";
+
+
+
+
+
+
+
+
+
+
+
+
+}
