6e723ed6a7a8817a51bc3b3efe635d479660f0a7
quaziarafeen <shamseelmail1@gmail.com>
2020-02-05 16:23:37 -0500

    New method added in Web API

--- CommerceInsurance/src/main/java/home/webelement/WebElementHomePage.java
+++ CommerceInsurance/src/main/java/home/webelement/WebElementHomePage.java
@@ -6,9 +6,9 @@ public class WebElementHomePage {
 // All the Web Elements comes in this class from HomePage
 
 
-    public static final String webElementSearchBox="//*[@id=\"twotabsearchtextbox\"]";
-    public static final String webElementSearchButton="//*[@id=\"nav-search\"]/form/div[2]/div/input";
-
+   // public static final String webElementSearchBox="//*[@id=\"twotabsearchtextbox\"]";
+    //public static final String webElementSearchButton="//*[@id=\"nav-search\"]/form/div[2]/div/input";
+    public static final String webElementGoForward = "//*[@id=\"main\"]/div/div[3]/div/div[2]/article/div/h2/a";
 
 
 
